8bb2141649729d70ce33d3486bfa266d02ef638e
bsaygili <bhdrsaygili@gmail.com>
2022-08-22 10:46:27 +0300

    delete reducers

--- src/reducers/restReducer.js
+++
@@ -1,8 +0,0 @@
-export const restReducer = (state, action) => {
-  switch (action.type) {
-    case "ADD_RESTAURANT":
-      return { ...state, restaurants: [...state, action.payload] };
-    default:
-      break;
-  }
-};
